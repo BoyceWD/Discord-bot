394ac4905088e197da9c70e7868a9d8f00913d84
Boyce Davis <boyce.w.davis@gmail.com>
2023-03-29 13:06:54 -0500

    added on_ready function

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import discord
 
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    #the 0 in {0.user} is replaced by value of client variable
+    print('We have logged in as {0.user}'.format(client))
